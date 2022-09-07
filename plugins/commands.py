24896045298f06cb085041bb3de5aba07fbb0cb8
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-09-07 16:43:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( 
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('subscribe', url='youtube.com/OpusTechz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
