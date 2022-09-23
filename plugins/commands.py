5b3c3e7f6c64ce0cb3e73f705e81e97fb1704c84
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-09-23 11:08:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -85,7 +85,7 @@ async def start(client, message):
             InlineKeyboardButton('⚚ ᗩᗞᗞ ᗰᗴ Ꭲᝪ Ꭹᝪᑌᖇ Ꮐᖇᝪᑌᑭ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🌿 𝐆𝐫𝐨𝐮𝐩 🌿', url='https://t.me/MaSTeR_filims'),
-            InlineKeyboardButton('💥  𝐨𝐰𝐧𝐞𝐫 💥', ', url='https://t.me/Master_brooi')
+            InlineKeyboardButton('💥  𝐨𝐰𝐧𝐞𝐫 💥',  url='https://t.me/Master_brooi')
             ],[      
             InlineKeyboardButton('♻️ 𝐇𝐞𝐥𝐩 ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ 𝐀𝐛𝐨𝐮𝐭 ♻️', callback_data='about')
