eade7dc11a49d1ddde8d893dd1fdbfc4aadab389
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-23 18:12:04 +0530

    Update ocr.py

--- plugins/admemes/ocr.py
+++ plugins/admemes/ocr.py
@@ -1,1 +1,14 @@
 
+
+from telegram import ChatAction,InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext.dispatcher import run_async
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackQueryHandler,PicklePersistence
+import logging
+import os
+from functools import wraps
+import requests
+
+API_KEY = os.environ.get("API_KEY","") 
+
+def send_typing_action(func)
+    """Sends typing action while processing func command."""
