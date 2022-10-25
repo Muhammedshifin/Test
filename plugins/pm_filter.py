6ac599938644652dbff9cdf3adaa6035cf0397fc
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-25 07:34:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1212,13 +1212,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "deploy":
+    elif query.data == "crush":
         buttons = [[
             InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.DEPLOY_TXT,
+            text=script.CRUSH_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
