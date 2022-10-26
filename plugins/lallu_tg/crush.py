c47f62deeffb1e04df3274370c1d431f8e830334
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-26 17:30:17 +0530

    Update crush.py

--- plugins/lallu_tg/crush.py
+++ plugins/lallu_tg/crush.py
@@ -30,10 +30,20 @@ async def aboutcrush(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-           photo=(PHOTO),
+           photo=(PHOTO)
            caption=script.CRUSHP_TXT.format(message.from_user.mention),
            reply_markup=reply_markup,
         )
 
-
-
+@Client.on_message(filters.command("crush"))
+async def aboutcrush(client, message):
+        buttons= [[
+             InlineKeyboardButton('OWNER', url='https')
+             ],[
+             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐',callback_data='close_data')
+        ]]
+        reply_markup = InlineKeybordMarkup(buttons)
+        await message. reply_photo(
+           photo=()
+           caption=script.NOTHING.format(message.from_user.mention),
+        )
