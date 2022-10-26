c1fd012c626749029a7e314995bacf0ecd1e866d
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-26 19:03:41 +0530

    Update crush.py

--- plugins/lallu_tg/crush.py
+++ plugins/lallu_tg/crush.py
@@ -20,23 +20,6 @@ Thank You"""
 PHOTO1 = "https://telegra.ph/file/70f9918fad55aea9b43fe.jpg"
 # -- Constants End -- #
 
-
-@Client.on_message(filters.command("crush"))
-async def aboutcrush(client, message):
-        buttons= [[
-             InlineKeyboardButton('♥️ 𝐅𝐚𝐭𝐡𝐢𝐦𝐚 ♥️', url='https://t.me/File_store_MsT_Bot')
-             ],[
-             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
-             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=(PHOTO)
-            caption=script.ABOUT_TXT.format(message.from_user.mention)
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-
 @Client.on_message(filters.command("owner"))
 async def aboutcrush(client, message):
         buttons= [[
