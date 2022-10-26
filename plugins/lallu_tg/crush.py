4481640f2c2434094d1ebb6284758617a6be7dd7
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-26 18:59:51 +0530

    Update crush.py

--- plugins/lallu_tg/crush.py
+++ plugins/lallu_tg/crush.py
@@ -32,7 +32,7 @@ async def aboutcrush(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=(PHOTO)
-            caption=script.CRUSHP_TXT.format(message.from_user.mention),
+            caption=script.ABOUT_TXT.format(message.from_user.mention)
             reply_markup=reply_markup,
             parse_mode='html'
         )
