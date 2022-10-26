cc50726d6b331674157d326ffdfef429a90eeb9d
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-26 21:27:23 +0530

    Update crush.py

--- plugins/lallu_tg/crush.py
+++ plugins/lallu_tg/crush.py
@@ -1,1 +1,11 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from Script import script
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+import logging
 
+PHOTO = 
+
+@Client.on_message(filters.command("crush"))
+async def aboutme(client, message):
