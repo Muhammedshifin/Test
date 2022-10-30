54132b284270518c1c0f67fe5f8fba677a77715b
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-10-30 08:54:41 +0530

    Update ocr.py

--- plugins/Don/ocr.py
+++ plugins/Don/ocr.py
@@ -1,1 +1,37 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+@Client.on_message(
+    filters.command("telegraph") &
+    f_onw_fliter
+)
+async def telegraph(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
+        return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
