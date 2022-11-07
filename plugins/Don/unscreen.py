ce24e1a9dda67a0097be7d222bd66b5e51b4eb48
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-11-07 21:00:39 +0530

    Update unscreen.py

--- plugins/Don/unscreen.py
+++ plugins/Don/unscreen.py
@@ -12,9 +12,7 @@ UNSCREEN_API = os.environ.get("UNSCREEN_API", "")
 
 Bot = Client(
     "Remove Background Bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
+    
 )
 
 START_TEXT = """Hello {},
