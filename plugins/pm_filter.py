134bd873fe2db669edf29f62300c2fb28b180b65
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-11-26 20:29:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -996,10 +996,22 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "stickerg":
         buttons = [[
             InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='hellp')
+            InlineKeyboardButton('ᴄᴏᴍᴍᴀɴᴅs', callback_data='sticercomm')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.STICKERG_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )  
+    elif query.data == "sticercomm":
+        buttons = [[
+            InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='stickerg')
+            
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.STICKERCOMMANDS,
             reply_markup=reply_markup,
             parse_mode='html'
         )  
