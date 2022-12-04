8780f2dc8e0859b7e4f2fbc00d8b996a7e662f48
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-12-04 11:37:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1552,7 +1552,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b><i>𝙃𝙚𝙧𝙚 𝙞𝙨 𝙬𝙝𝙖𝙩 𝙞𝙨 𝙛𝙤𝙪𝙣𝙙 𝙮𝙤𝙪𝙧 𝙦𝙪𝙚𝙧𝙮:{search}\n👤𝙍𝙚𝙦𝙪𝙚𝙨𝙩𝙚𝙙 𝘽𝙮 : {message.from_user.mention}\n👥𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗯𝘆 : {message.chat.title}</i></b>"
+        cap = f"<b>𝙃𝙚𝙧𝙚 𝙞𝙨 𝙬𝙝𝙖𝙩 𝙞𝙨 𝙛𝙤𝙪𝙣𝙙 𝙮𝙤𝙪𝙧 𝗠𝗼𝗩𝗶𝗲:{search}\n👤𝙍𝙚𝙦𝙪𝙚𝙨𝙩𝙚𝙙 𝘽𝙮: {message.from_user.mention}\n👥𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗯𝘆: {message.chat.title}</b>"
  
     if imdb and imdb.get('poster'):
         try:
