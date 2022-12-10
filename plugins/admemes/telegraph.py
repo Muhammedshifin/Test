c4cb7f9d5e374dc1f1bfbae40aefc49b2fca4139
Muhammedshifin <112153944+Muhammedshifin@users.noreply.github.com>
2022-12-10 09:05:33 +0530

    Update telegraph.py

--- plugins/admemes/telegraph.py
+++ plugins/admemes/telegraph.py
@@ -7,47 +7,37 @@ from plugins.helper_functions.cust_p_filters import f_onw_fliter
 from plugins.helper_functions.get_file_id import get_file_id
 
 
-@Client.on_message(
-    filters.command("telegraph") &
-    f_onw_fliter
-)
-async def telegraph(client, message):
-    replied = message.reply_to_message
+@Client.on_message(filters.command("telegraph") & filters.private)
+async def telegraph_upload(bot, update):
+    FSub = await ForceSub(bot, update)
+    if FSub == 400:
+        return
+    replied = update.reply_to_message
     if not replied:
-        await message.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
+        await update.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
         return
-    file_info = get_file_id(replied)
-    if not file_info:
-        await message.reply_text("Not supported!")
-        return
-    _t = os.path.join(
-        TMP_DOWNLOAD_DIRECTORY,
-        str(replied.message_id)
-    )
-    if not os.path.isdir(_t):
-        os.makedirs(_t)
-    _t += "/"
-    download_location = await replied.download(
-        _t
-    )
+    text = await update.reply_text(text="<code>Downloading to My Server ...</code>", disable_web_page_preview=True)   
+    media = await update.reply_to_message.download()   
+    await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)                                            
     try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply_text(message, text=document)
-    else:
-        await message.reply(
-            f"Link :- <code>https://graph.org{response[0]}</code>",
-            disable_web_page_preview=True
-            reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
-                InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
-                ],[
-                InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
-                ]]
-            )
+        response = upload_file(media)
+    except Exception as error:
+        print(error)
+        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)       
+        return    
+    try:
+        os.remove(media)
+    except Exception as error:
+        print(error)
+        return    
+    await text.edit_text(
+        text=f"<b>Link :-</b>\n\n<code>https://graph.org{response[0]}</code>",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
+            InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
+            ],[
+            InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
+            ]]
         )
-    finally:
-        shutil.rmtree(
-            _t,
-            ignore_errors=True
-        )
+    )
