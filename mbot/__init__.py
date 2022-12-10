98c2a820f36f7e0b7000860253b096ffa4c40af8
Masterolic <rockingmallu8@gmail.com>
2022-12-10 19:12:02 +0530

    Create __init__.py

---
+++ mbot/__init__.py
@@ -0,0 +1,31 @@
+from os import environ,sys,mkdir,path
+import logging
+from sys import executable
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(message)s",
+    handlers = [logging.FileHandler('bot.log'), logging.StreamHandler()]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+try:
+    API_ID = int(environ['API_ID'])
+    API_HASH = environ['API_HASH']
+    BOT_TOKEN = environ['BOT_TOKEN']
+    OWNER_ID = int(environ['OWNER_ID'])
+except KeyError:
+# Optional Variable
+SUDO_USERS = environ.get("SUDO_USERS",str(OWNER_ID)).split()
+SUDO_USERS = [int(_x) for _x in SUDO_USERS]
+if OWNER_ID not in SUDO_USERS:
+    SUDO_USERS.append(OWNER_ID)
+AUTH_CHATS = environ.get('AUTH_CHATS',None ).split()
+AUTH_CHATS = [int(_x) for _x in AUTH_CHATS]
+LOG_GROUP = environ.get("LOG_GROUP", None)
+if LOG_GROUP:
+    LOG_GROUP = int(LOG_GROUP)
+BUG = environ.get("BUG", None)
+if BUG:
+    BUG = int(BUG)
